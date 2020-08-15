b6d636f4d35c2f4a416bdb7f34272d2d176afd01
Oriol Bover <bover73@gmail.com>
2020-08-15 19:53:35 +0200

    add dark mode 🌓

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,9 @@
 import style from 'styles/components/header.module.css'
+import useTheme from 'hooks/useTheme'
 
 export default function Header() {
+  const { toggleTheme } = useTheme()
+
   return (
     <header className={style.header}>
       <div className={style.inner}>
@@ -8,6 +11,9 @@ export default function Header() {
           <span className={style.mark}>{'>'}</span>
           <span className={style.text}> Oriol Bover</span>
           <span className={style.cursor} />
+        </div>
+        <div className={style.right}>
+          <button onClick={toggleTheme}>Theme</button>
         </div>
       </div>
     </header>
---
+++ hooks/useTheme.js
@@ -0,0 +1,60 @@
+import { useCallback, useEffect } from 'react'
+import useSWR from 'swr'
+
+export const themeStorageKey = 'theme'
+
+const isServer = typeof window === 'undefined'
+
+const getTheme = () => {
+  if (isServer) return 'dark'
+  return localStorage.getItem(themeStorageKey) || 'dark'
+}
+
+const setLightMode = () => {
+  try {
+    localStorage.setItem(themeStorageKey, 'light')
+    document.documentElement.classList.remove('dark')
+    document.documentElement.classList.add('light')
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+const setDarkMode = () => {
+  try {
+    localStorage.setItem(themeStorageKey, 'dark')
+    document.documentElement.classList.remove('light')
+    document.documentElement.classList.add('dark')
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+const useTheme = () => {
+  const { data: theme, mutate } = useSWR(themeStorageKey, getTheme, {
+    initialData: getTheme(),
+  })
+
+  const setTheme = useCallback(
+    (newTheme) => {
+      mutate(newTheme, false)
+    },
+    [mutate]
+  )
+
+  useEffect(() => {
+    if (theme === 'dark') {
+      setDarkMode()
+    } else {
+      setLightMode()
+    }
+  }, [theme])
+
+  return {
+    theme,
+    setTheme,
+    toggleTheme: () => setTheme(!theme || theme === 'dark' ? 'light' : 'dark'),
+  }
+}
+
+export default useTheme
